26cea997676694c45eac7a50f9457882aea3e3fc
Your Name <you@example.com>
2020-07-10 01:49:47 +0800

    update0710long

--- th5/application/acomplain/controller/Index.php
+++ th5/application/acomplain/controller/Index.php
@@ -1,57 +1,10 @@
 <?php
 namespace app\acomplain\controller;
 
-use think\Controller;
-use think\Db;
-use think\Session;
-use app\base\controller\ModuleBaseController;
-
-class Index extends ModuleBaseController
+class Index
 {
-    // 查询数据
     public function index()
-    {   
-        $result= db('comment c,peanut_user u,peanut_user d')
-        ->field('c.*,u.name as uName,d.name as dName,d.credit as dCredit')
-        ->where('c.user_id = u.id and c.sell_id = d.id')
-        ->order('id desc')
-        ->select();
-        if ($result) {
-            echo json_encode($this->actionSuccess($result,0,'成功'));
-        } else {
-            echo json_encode($this->actionFail('失败'));
-        }
-    }
-
-
-    // 修改数据
-    public function update()
     {
-        $id = getPost()['id'];
-        $credit = getPost()['credit'];
-        $where = [
-            'id' => $id
-        ];
-        $update = [
-            'credit' => $credit
-        ];
-        $update2 = [
-            'comment_state' => '已审'
-        ];
-        $result = db('user')->where($where)->update($update);
-        $result2 = db('comment')->where($where)->update($update2);
-        if ($result && $result2) {
-            echo json_encode($this->actionSuccess($result,1,'成功'));
-        } else {
-            echo json_encode($this->actionFail('失败'));
-        }  
+        return '<style type="text/css">*{ padding: 0; margin: 0; } div{ padding: 4px 48px;} a{color:#2E5CD5;cursor: pointer;text-decoration: none} a:hover{text-decoration:underline; } body{ background: #fff; font-family: "Century Gothic","Microsoft yahei"; color: #333;font-size:18px;} h1{ font-size: 100px; font-weight: normal; margin-bottom: 12px; } p{ line-height: 1.6em; font-size: 42px }</style><div style="padding: 24px 48px;"> <h1>:)</h1><p> ThinkPHP V5<br/><span style="font-size:30px">十年磨一剑 - 为API开发设计的高性能框架</span></p><span style="font-size:22px;">[ V5.0 版本由 <a href="http://www.qiniu.com" target="qiniu">七牛云</a> 独家赞助发布 ]</span></div><script type="text/javascript" src="http://tajs.qq.com/stats?sId=9347272" charset="UTF-8"></script><script type="text/javascript" src="http://ad.topthink.com/Public/static/client.js"></script><thinkad id="ad_bd568ce7058a1091"></thinkad>';
     }
-
-
-    
-
-
-   
-
-
 }
